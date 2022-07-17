c915b94f60c157506d79380235449b5aeb5479bd
pratyuds <pratyu.ds1117@gmail.com>
2022-07-17 19:27:16 +0530

    this is my first code push

---
+++ mangotest.py
@@ -0,0 +1,16 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://ineuron_pratyu:Chahit1117@pratyusha.dc3s9sa.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+d = {
+    "name" : "pratyusha",
+    "email" : "pratyusha@gmail.com",
+    "surname" : "ghanta"
+}
+
+db1 = client['mongotest']
+col1 = db1['test']
+col1.insert_one(d)
